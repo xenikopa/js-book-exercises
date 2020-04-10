9add4cf50f7f6acddb1062e6c8cc58ad992858bb
Kseniia <xeni.kopa@icloud.com>
2020-04-10 09:49:51 +0300

    fix pr issuse

--- scripts/server.js
+++ scripts/server.js
@@ -23,7 +23,7 @@ let createServer = (callback) => (port) => http
  * @param {http responce} response 
  */
 let getStream = response => filename => fs
-    .createReadStream(`./${filename}`)
+    .createReadStream(filename)
     .on('error', () => {
         console.log(color('red'), "Error! Can't read file 'index.html'. Check, that file is exists & correct.");
         response.end();
@@ -32,40 +32,36 @@ let getStream = response => filename => fs
 let serverCallback = (filename) => (request, response) => getStream(response)(filename).pipe(response);
 
 let showHelp = () => void console.log(`
-Welcome to server.js script! This script run the server.
+Usage: node server [options]
 
-You can use agruments: 
+Options: 
 
---port - to set port for server. 
-    Default value: 8080. 
-    Example: --port 3000.
-
---file - to set file name for start page. 
-    Default value: index.html. 
-    Example: --file start.html
+--port  set port for server. Default value: 8080. Example: --port 3000.
+--file  set file name for start page. Default value: index.html. Example: --file start.html
+--help, -h  output usage information
 `);
 
 /**
  * 
  * @param {process.argv} agrv 
  */
-let getPort = (agrv) => pipe(
+let getPort = pipe(
     agrv => agrv.findIndex(x => x === '--port'),
     index => index === -1 ? null : Number(agrv[index + 1]),
-    index => {
-        if (isNaN(index)){
-            console.error(color('red'), 'Index must be a Number.  For more information use --help');
+    port => {
+        if (isNaN(port)){
+            console.error(color('red'), 'Port must be a Number. For more information use --help');
             process.exit(1);
         }
-        return index || 3000;
+        return port || 3000;
     }
-)(agrv);
+);
 
 /**
  * 
  * @param {process.argv} agrv 
  */
-let getFile = (argv) => pipe(
+let getFile = pipe(
     argv => argv.findIndex(x => x === '--file'),
     index => index === -1 ? null : argv[index + 1],
     filename => {
@@ -75,20 +71,29 @@ let getFile = (argv) => pipe(
         }
         return filename || 'index.html'
     }
-)(argv);
+);
 
-let getHelp = (argv) => pipe(
-    argv => argv.findIndex(x => x === '--help'),
+let getHelp = pipe(
+    argv => argv.findIndex(x => x === '--help' || x === '-h'),
     index => {
         if (index === -1) { return; }
         showHelp();
         process.exit(0);
     }
-)(argv);
+);
 
 void function() {
-    let agrv = process.argv.slice(2);
-    getHelp(agrv);
-    let port = getPort(agrv);
-    createServer(serverCallback(getFile(agrv)))(port).listen(port)
+    let argv = process.argv.slice(2);
+
+    getHelp(argv);
+
+    let port = getPort(argv);
+
+    let server = pipe(
+        getFile,
+        serverCallback,
+        createServer,
+    )(argv)
+      
+    server(port).listen(port)
 }()
